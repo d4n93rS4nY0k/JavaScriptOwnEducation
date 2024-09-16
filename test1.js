82dbbfae108988697e41d4ec2a618eb94efc0c0a
денжасанек <137263380+d4n93rS4nY0k@users.noreply.github.com>
2024-09-16 16:30:44 +0300

    Add files via upload

---
+++ test1.js
@@ -0,0 +1,16 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+    console.log('Client connected');
+
+    ws.on('message', function incoming(message) {
+        console.log('Received: %s', message);
+        ws.send(`${message}`);
+    });
+
+    ws.on('close', function () {
+        console.log('Client disconnected');
+    });
+});
